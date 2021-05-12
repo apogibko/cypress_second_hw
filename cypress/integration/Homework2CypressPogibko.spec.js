fcab6c468897c38ef907c7782eec1a0cb19f086b
apogibko <apogibko@gmail.com>
2021-05-12 16:23:13 +0300

    second+ commit

--- cypress/integration/Homework2CypressPogibko.spec.js
+++ cypress/integration/Homework2CypressPogibko.spec.js
@@ -2,25 +2,27 @@
 
 it ('Log with valid creds', () => {
     cy.visit('https://the-internet.herokuapp.com/login');
-    cy.get('[name="username"]').type('tomsmith').should('have.value', 'tomsmith');
-    cy.get('[type="password"]').type('SuperSecretPassword!').should('have.value', 'SuperSecretPassword!');
-    cy.get('[class="fa fa-2x fa-sign-in"]').click();
+    cy.get('#username').type('tomsmith').should('have.value', 'tomsmith');
+    cy.get('#password').type('SuperSecretPassword!').should('have.value', 'SuperSecretPassword!');
+    cy.get('.fa').click();
+    cy.get('#flash').should('contain', 'You logged into a secure area!');
 });
 
 it ('Log out', () => {
     cy.visit('https://the-internet.herokuapp.com/login');
-    cy.get('[name="username"]').type('tomsmith').should('have.value', 'tomsmith');
-    cy.get('[type="password"]').type('SuperSecretPassword!').should('have.value', 'SuperSecretPassword!');
-    cy.get('[class="fa fa-2x fa-sign-in"]').click();
-    cy.get('[class="button secondary radius"]').click();
+    cy.get('#username').type('tomsmith').should('have.value', 'tomsmith');
+    cy.get('#password').type('SuperSecretPassword!').should('have.value', 'SuperSecretPassword!');
+    cy.get('.fa').click();
+    cy.get('.button').click();
+    cy.get('#flash').should('contain', 'You logged out of the secure area!');
 });
 
 it ('Log with invalid creds', () => {
     cy.visit('https://the-internet.herokuapp.com/login');
     cy.get('[name="username"]').type('tomsmith1').should('have.value', 'tomsmith1');
     cy.get('[type="password"]').type('SuperSecretPassword!1').should('have.value', 'SuperSecretPassword!1');
-    cy.get('[class="fa fa-2x fa-sign-in"]').click();
-    cy.get('[class="flash error"]').should('exist');
+    cy.get('.fa').click();
+    cy.get('#flash').should('contain', 'Your username is invalid!');
 });
 
 
